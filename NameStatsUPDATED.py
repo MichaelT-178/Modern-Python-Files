b1635a4e8a1b6bff05083a4aff3c00590522ff18
Michael Totaro <mtotaro@ncsu.edu>
2023-10-05 01:55:26 -0400

    Removed comment

--- NameStatsUPDATED.py
+++ NameStatsUPDATED.py
@@ -1,5 +1,4 @@
 # Updated version of NameStats.py. Written on 4/13/2023. Original was ~136 lines, this is ~35 lines.
-# This is not connected with og_main_algorithm in anyway.
 og_name = input("Please enter your full name: ").strip()
 
 while len(og_name.split(" ")) != 3:
