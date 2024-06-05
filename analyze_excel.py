be686c1bdf23f541a4f221d2127a6364b919904e
Michael Totaro <mstotaro@gmail.com>
2024-06-05 14:28:22 -0400

    Analayze excel

--- analyze_excel.py
+++ analyze_excel.py
@@ -1,16 +1,32 @@
 import pandas as pd
+import numpy as np
+import openpyxl
 
+
+path = ""
 excel_path = "spread_sheet.xlsx"
 
-df = pd.read_excel(excel_path, engine="openpyxl")
+full_path = f"{path}/{excel_path}"
+
+
+df = pd.read_excel(full_path, engine="openpyxl")
+
 
 # print(df['Unnamed: 0'])
 
-
-count = 0
-
 # df.head() Used to see data if you can't find column name
 
-for cool in df['Unnamed: 5']:
-    if isinstance(cool, str):
-        print(cool)
+columns = ['Unnamed: 5', 'Unnamed: 6', 'Unnamed: 7']
+
+col_data = [df[col].tolist() for col in columns]
+
+header = "\t".join(columns)
+
+
+for row in zip(*col_data):
+
+   filtered_row = [str(value) for value in row if not pd.isna(value) and value != ""]
+
+   print("\t\t\t".join(map(str, filtered_row)))
+
+   
